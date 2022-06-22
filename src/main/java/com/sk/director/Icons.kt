9615bda45ad044e2eb656dbbf71f66ae5a1bdfc6
Swapnil Kadlag <swapnil.kadlag.27@gmail.com>
2022-06-22 22:09:00 +0530

    Code refactored (removed iconLoader deprecation)

--- src/main/java/com/sk/director/Icons.kt
+++ src/main/java/com/sk/director/Icons.kt
@@ -3,6 +3,9 @@ package com.sk.director
 import com.intellij.openapi.util.IconLoader
 
 object Icons {
-    val Table = IconLoader.getIcon("/icons/table.svg")
-    val Key = IconLoader.getIcon("/icons/key.svg")
+    @JvmField
+    val Table = IconLoader.getIcon("/icons/table.svg", javaClass)
+
+    @JvmField
+    val Key = IconLoader.getIcon("/icons/key.svg", javaClass)
 }
